92e2119e8350b6ebf7ded0fe6d00626ccefe0d09
alv-cj <alvaro.valverde@funcionjudicial.gob.ec>
2024-10-10 08:07:14 -0500

    un componente

---
+++ src/app/barra-superior/barra-superior.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-barra-superior',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule,MatMenuModule],
+  templateUrl: './barra-superior.component.html',
+  styleUrl: './barra-superior.component.css'
+})
+export class BarraSuperiorComponent {
+
+}
